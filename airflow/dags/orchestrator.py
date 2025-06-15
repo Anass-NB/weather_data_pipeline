23a2764f44546ac73ab3ce610cae2ea596220e03
Anass-NB <Anassnabil067@gmail.com>
2025-06-15 20:39:36 +0100

    add DAG for DBT

--- airflow/dags/orchestrator.py
+++ airflow/dags/orchestrator.py
@@ -2,7 +2,16 @@ from datetime import datetime,timedelta
 from airflow.operators.python import PythonOperator
 from airflow.sdk import DAG
 import sys
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount  # ✅ This is the Mount class
+
+
+
+
+
 sys.path.append("/opt/airflow/api")
+
+
 default_args = {
     "description" : "A DAG to orchestrate Data",
     "start_date" :datetime(2025,6,13),
@@ -11,6 +20,8 @@ default_args = {
 def safe_main_callable():
     from insert_records import main
     return main()
+
+
 dag = DAG (
     dag_id = "weather_api",
     default_args =default_args ,
@@ -19,10 +30,21 @@ dag = DAG (
 
 
 with dag:
-    #task1
     task1 = PythonOperator(
         task_id = "fetch_data_task1",
         python_callable = safe_main_callable
     )
-    #task2
-    #task3
+    task2 = DockerOperator(
+        task_id='transform_data_task',
+        image='ghcr.io/dbt-labs/dbt-postgres:1.9.latest',
+        working_dir='/usr/app',
+        command="run --select stg_weather_data",
+        mounts = [
+            Mount(source="/home/anass/repos/weather-data-project/dbt/my_project",target="/usr/app",type="bind"),
+            Mount(source="/home/anass/repos/weather-data-project/dbt/profiles.yml",target="/root/.dbt/profiles.yml",type="bind"),
+            ],
+        docker_url='unix://var/run/docker.sock',  # Unix socket for Docker
+        network_mode='weather-data-project_mynetwork',
+        auto_remove='success'
+    )
+    task1 >> task2
